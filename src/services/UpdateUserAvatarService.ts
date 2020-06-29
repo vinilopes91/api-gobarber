ff4c4798b83fb905def52ef48c1378f0f7dcfc0e
vinilopes91 <vinilopes13@hotmail.com>
2020-06-29 01:01:13 -0300

    Update avatar service

---
+++ src/services/UpdateUserAvatarService.ts
@@ -0,0 +1,41 @@
+import path from 'path';
+import fs from 'fs';
+import { getRepository } from 'typeorm';
+
+import User from '../models/User';
+
+import uploadConfig from '../config/upload';
+
+interface Request {
+  user_id: string;
+  avatarFileName: string;
+}
+
+class UpdateUserAvatarService {
+  public async execute({ user_id, avatarFileName }: Request): Promise<User> {
+    const userRespository = getRepository(User);
+
+    const user = await userRespository.findOne(user_id);
+
+    if (!user) {
+      throw new Error('Only authenticated users can change avatar.');
+    }
+
+    if (user.avatar) {
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+
+      if (userAvatarFileExists) {
+        fs.promises.unlink(userAvatarFilePath);
+      }
+    }
+
+    user.avatar = avatarFileName;
+
+    await userRespository.save(user);
+
+    return user;
+  }
+}
+
+export default UpdateUserAvatarService;
